efd7440f2ab214b4951c491f6cd2f185a82aefd9
Purplelops <purplelops@gmail.com>
2024-05-07 20:22:53 +0200

    Score system and restart system

--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var death_effect_scene: PackedScene = load("res://death_effect.tscn")
+var score: int = 0
 
 func _ready() -> void:
 	randomize()
@@ -9,6 +10,7 @@ func _ready() -> void:
 func _on_player_area_entered(area: Area3D) -> void:
 	if $Player.alive:
 		_die()
+		$RestartButton.visible = true
 	else:
 		pass
 
@@ -24,3 +26,13 @@ func _die() -> void:
 	
 	$Player.alive = false
 	$Player.visible = false
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_score_timer_timeout() -> void:
+	if $Player.alive:
+		score += 1
+		$ScoreLabel.text = str(score)
